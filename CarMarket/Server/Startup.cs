6c7a94a22c55cbbcd6472b147661be034f145001
Vlad <vlad.doroh@gmail.com>
2021-07-07 14:03:54 +0300

    Added swagger.

--- CarMarket/Server/Startup.cs
+++ CarMarket/Server/Startup.cs
@@ -23,7 +23,6 @@ namespace CarMarket.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddSwaggerGen(c =>
