d4484788b86d92cb7f0255b7eb27e16e582800ec
Vlad <vlad.doroh@gmail.com>
2021-07-12 13:01:08 +0300

    Added car model.

---
+++ CarMarket.Core/Car/Repository/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CarMarket.Core.Car.Repository
+{
+    public interface ICarRepository
+    {
+        Task<CarModel> FindByIdAsync(int id);
+        Task<CarModel> FindByNameAsync(string email);
+        Task<long> SaveAsync(CarModel user);
+        Task<List<CarModel>> FindAllAsync();
+    }
+}
