9e95458255b632477325a8e90f8224a68779d057
Vlad <vlad.doroh@gmail.com>
2021-08-02 16:26:55 +0300

    Updated User model.

--- CarMarket.Core/User/Domain/UserModel.cs
+++ CarMarket.Core/User/Domain/UserModel.cs
@@ -1,15 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
-
-namespace CarMarket.Core.User.Domain
+﻿namespace CarMarket.Core.User.Domain
 {
-    public class UserModel : IdentityUser
+    public class UserModel
     {
-        //public long Id { get; set; }
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        //public string Email { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
         //public ICollection<Permission> Permissions { get; set; }
-        //public string Password { get; set; }
     }
 }
