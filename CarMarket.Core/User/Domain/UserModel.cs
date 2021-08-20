abae960ec618687bfe3eefd183bdd0e85bae4a26
Vlad <vlad.doroh@gmail.com>
2021-08-20 13:33:27 +0300

    Code cleanup.

--- CarMarket.Core/User/Domain/UserModel.cs
+++ CarMarket.Core/User/Domain/UserModel.cs
@@ -4,12 +4,8 @@ namespace CarMarket.Core.User.Domain
 {
     public class UserModel : IdentityUser
     {
-        //public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public IdentityRole<string> Role { get; set; }
-        //public string Email { get; set; }
-        //public string PasswordHash { get; set; }
-        //public ICollection<Permission> Permissions { get; set; }
     }
 }
